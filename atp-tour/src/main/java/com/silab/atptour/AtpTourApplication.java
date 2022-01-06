76b9c4dceecc37ce125aff6e55b065c4a43df97f
Lazar Dondo <lazardondo5@gmail.com>
2022-01-06 15:41:53 +0100

    #1 init

---
+++ atp-tour/src/main/java/com/silab/atptour/AtpTourApplication.java
@@ -0,0 +1,13 @@
+package com.silab.atptour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtpTourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtpTourApplication.class, args);
+	}
+
+}
