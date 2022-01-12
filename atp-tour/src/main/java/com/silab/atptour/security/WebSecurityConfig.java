d34f35ba896b4c307e629bb23764e29ad2a6acf6
Lazar Dondo <lazardondo5@gmail.com>
2022-01-12 16:50:42 +0100

    #8 Configure basic authentication using Spring Security

--- atp-tour/src/main/java/com/silab/atptour/security/WebSecurityConfig.java
+++ atp-tour/src/main/java/com/silab/atptour/security/WebSecurityConfig.java
@@ -1,9 +1,16 @@
 package com.silab.atptour.security;
 
+import com.silab.atptour.service.impl.UserServiceImpl;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 /**
  *
@@ -13,9 +20,52 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final String defaultUser = "USER";
+
+    private final String adminUser = "ADMIN";
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new UserServiceImpl();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(authenticationProvider());
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
+        http.csrf().disable().authorizeRequests()
+                //user
+                .antMatchers(HttpMethod.PUT, "/user").hasAuthority("ADMIN")
+                .antMatchers("/user/**").permitAll()
+                //tournament  
+                .antMatchers(HttpMethod.DELETE, "/tournament/**").hasAuthority(adminUser)
+                .antMatchers(HttpMethod.GET, "/tournament/**").hasAnyAuthority(adminUser, defaultUser)
+                .antMatchers("/tournament").hasAuthority(adminUser)
+                //player   
+                .antMatchers(HttpMethod.GET, "/player/**").hasAnyAuthority(adminUser, defaultUser)
+                .antMatchers("/player").hasAuthority(adminUser)
+                //matches
+                .antMatchers(HttpMethod.GET, "/matches/**/**/").hasAnyAuthority(adminUser, defaultUser)
+                .antMatchers("/matches").hasAuthority(adminUser)
+                //statistics
+                .antMatchers("/statistics").hasAuthority(adminUser)
+                .anyRequest().authenticated()
+                .and().httpBasic();
     }
 }
