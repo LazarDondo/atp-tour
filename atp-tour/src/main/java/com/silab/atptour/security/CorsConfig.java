830715b6451c9eed32405e6b1808f00c1d9cd8db
Lazar Dondo <lazardondo5@gmail.com>
2022-01-13 22:14:20 +0100

    #8 Add CORS configuration

---
+++ atp-tour/src/main/java/com/silab/atptour/security/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.silab.atptour.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ *
+ * @author Lazar
+ */
+@Configuration
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Value("${cors.allowed.url}")
+    private String allowedUrl;
+    
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins(allowedUrl);
+    }
+}
