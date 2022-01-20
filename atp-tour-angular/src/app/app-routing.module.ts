ccb3a82f276afb6ed5218a717d6fb605ecd3ce6f
Lazar Dondo <lazardondo5@gmail.com>
2022-01-20 18:36:52 +0100

    #10 Add registration page

--- atp-tour-angular/src/app/app-routing.module.ts
+++ atp-tour-angular/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginPageComponent } from './features/login-page/login-page.component';
+import { RegistrationPageComponent } from './features/registration-page/registration-page/registration-page.component';
 
 const routes: Routes = [
   {path:'login',component:LoginPageComponent},
+  {path:'register',component:RegistrationPageComponent},
   {path:'',redirectTo:'/login', pathMatch:'full'},
 ];
 
---
+++ atp-tour-angular/src/app/features/registration-page/registration-page/registration-page.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UserService } from 'src/app/core/services/user.service';
+import { User } from 'src/app/models/user.model';
+
+@Component({
+  selector: 'app-registration-page',
+  templateUrl: './registration-page.component.html',
+  styleUrls: ['./registration-page.component.scss']
+})
+export class RegistrationPageComponent implements OnInit {
+  registerForm: FormGroup;
+  loading = false;
+  submitted = false;
+  registrationError = false;
+
+  constructor(private userService: UserService, private formBuilder: FormBuilder) {
+  }
+
+  ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      username: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]]
+    });
+  }
+
+  get f() {
+    return this.registerForm.controls;
+  }
+
+  onSubmit() {
+    this.submitted = true;
+
+    if (this.registerForm.invalid) {
+      return;
+    }
+
+    this.loading = true;
+    this.register();
+  }
+
+  register() {
+    this.userService.registerUser(this.registerForm.value).subscribe({
+      next: registeredUser => {
+        this.registrationError=false;
+        this.loading=false;
+      },
+       error: err => {
+        this.registrationError=true;
+        this.loading=false;
+      }
+    });
+  }
+}
