6dee4eb7242f8489243bb4ffbd42fa77de1eb34d
Lazar Dondo <lazardondo5@gmail.com>
2022-01-20 19:33:14 +0100

    #12 Add footer

--- atp-tour-angular/src/app/app.module.ts
+++ atp-tour-angular/src/app/app.module.ts
@@ -8,7 +8,7 @@ import { AuthInterceptor } from './core/interceptors/auth.interceptor';
 import { LoginPageComponent } from './features/login-page/login-page.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RegistrationPageComponent } from './features/registration-page/registration-page.component';
-import { HeaderComponent } from './features/header/header/header.component';
+import { HeaderComponent } from './features/header/header.component';
 import { FooterComponent } from './features/footer/footer.component';
 
 
