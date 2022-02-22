33901bcdc91b193bf082eaf3f612a56097862239
Lazar Dondo <lazardondo5@gmail.com>
2022-02-22 17:53:17 +0100

    #12 Update header and footer

--- atp-tour-angular/src/app/features/header/header.component.ts
+++ atp-tour-angular/src/app/features/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
   selector: 'atp-header',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  loggedIn = false;
+  userInfo: string;
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
+    this.loggedIn = this.authService.isLoggedIn();
+    if(this.loggedIn){
+      this.userInfo=this.authService.getUserInfo();
+    }
+  }
+
+  logout(){
+    sessionStorage.removeItem('loggedUser');
   }
 
 }
